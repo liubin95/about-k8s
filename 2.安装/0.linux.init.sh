8cc31167ce4629e87657f1c7cc50f1f03b963f63
liubin <liu_bin4835@outlook.com>
2024-06-15 18:41:13 +0800

    update 安装 绿色字体

--- 2.安装/0.linux.init.sh
+++ 2.安装/0.linux.init.sh
@@ -3,7 +3,7 @@ set -e
 
 # update
 dnf update -y
-echo 'update done'
+echo -e "\033[32m dnf update deno \033[0m"
 
 # 设置所需的 sysctl 参数，参数在重新启动后保持不变
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
@@ -14,22 +14,22 @@ net.ipv6.conf.all.forwarding        = 1
 EOF
 # 应用 sysctl 参数而不重新启动
 sudo sysctl --system
-echo '桥接流量 done'
+echo -e "\033[32m 桥接流量 deno \033[0m"
 
 timedatectl set-timezone Asia/Shanghai
 date -R
-echo '时区 done'
+echo -e "\033[32m 时区 deno \033[0m"
 
 systemctl disable --now firewalld
-echo 'firewall done'
+echo -e "\033[32m firewall deno \033[0m"
 
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-echo 'swap off done'
+echo -e "\033[32m swap off deno \033[0m"
 
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-echo 'selinux off done'
+echo -e "\033[32m selinux off deno \033[0m"
 
 # install containerd
 OS=linux
@@ -37,41 +37,41 @@ ARCH=amd64
 CONTAINERD_VERSION=1.7.18
 curl -OL https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-${OS}-${ARCH}.tar.gz
 tar Cxzvf /usr/local containerd-${CONTAINERD_VERSION}-${OS}-${ARCH}.tar.gz
-echo 'containerd done'
+echo -e "\033[32m containerd deno \033[0m"
 
 curl -L https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -o /etc/systemd/system/containerd.service
 systemctl daemon-reload
 systemctl enable --now containerd
-echo 'containerd service done'
+echo -e "\033[32m containerd service deno \033[0m"
 
 # install runc
 RUNC_VERSION=1.1.13
 curl -OL https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.${ARCH}
 install -m 755 runc.amd64 /usr/local/sbin/runc
-echo 'runc done'
+echo -e "\033[32m runc deno \033[0m"
 
 # cni 插件
 CNI_VERSION=1.5.0
 mkdir -p /opt/cni/bin
 curl -OL https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-${OS}-${ARCH}-v${CNI_VERSION}.tgz
 tar Cxzvf /opt/cni/bin cni-plugins-${OS}-${ARCH}-v${CNI_VERSION}.tgz
-echo 'cni done'
+echo -e "\033[32m cni deno \033[0m"
 
 # 配置 containerd
 mkdir -p /etc/containerd
 containerd config default > /etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 systemctl restart containerd
-echo 'containerd config done'
+echo -e "\033[32m containerd config deno \033[0m"
 
 VERSION="v1.26.0" # check latest version in /releases page
 curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-${VERSION}-linux-amd64.tar.gz --output crictl-${VERSION}-linux-amd64.tar.gz
 tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-$VERSION-linux-amd64.tar.gz
-echo 'crictl done'
+echo -e "\033[32m crictl deno \033[0m"
 
 dnf -y install conntrack socat
-echo 'conntrack  socat done'
+echo -e "\033[32m conntrack  socat deno \033[0m"
 
 RELEASE="v1.30.2"
 ARCH="amd64"
@@ -85,5 +85,5 @@ curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSIO
 mkdir -p /etc/systemd/system/kubelet.service.d
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 systemctl enable --now kubelet
-echo 'kubelet done'
+echo -e "\033[32m kubelet deno \033[0m"
 
