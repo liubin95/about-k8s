b41e74605a6fd4d85cb760fbf22490fb722e80b9
liubin <liu_bin4835@outlook.com>
2024-06-15 18:55:42 +0800

    update 安装 cdn

--- 2.安装/0.linux.init.sh
+++ 2.安装/0.linux.init.sh
@@ -77,7 +77,7 @@ RELEASE="v1.30.2"
 ARCH="amd64"
 DOWNLOAD_DIR="/usr/local/bin"
 cd $DOWNLOAD_DIR
-curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet,kubectl}
+curl -L --remote-name-all https://cdn.dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet,kubectl}
 chmod +x {kubeadm,kubelet,kubectl}
 
 RELEASE_VERSION="v0.16.2"
@@ -85,5 +85,5 @@ curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSIO
 mkdir -p /etc/systemd/system/kubelet.service.d
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | sudo tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 systemctl enable --now kubelet
-echo -e "\033[32m kubelet deno \033[0m"
+echo -e "\033[32m kube deno \033[0m"
 
